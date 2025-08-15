3243f520785b483e2d6a4e6c55f56dc98684b63c
yamasoph <sophiayamanishi@gmail.com>
2025-08-15 13:54:02 -0400

    Update recgrid.py

--- Data/recgrid.py
+++ Data/recgrid.py
@@ -9,10 +9,10 @@ h5_path = Path("reclongrun.h5")
 
 canvas_width_deg = 90 # canvas width in degrees
 canvas_height_deg = 180 # canvas height in degrees
-tile_width_deg =  0.765598739109 # actual angular degree size width
-tile_height_deg = 0.572099528312 # actual angular degree size height
-motor_step_deg = 0.75 # motor step size
-azimuth_step_deg = 5.0 # azimuth step size
+tile_width_deg = 0.921202100973 # actual angular degree size width
+tile_height_deg = 0.688377359072 # actual angular degree size height
+motor_step_deg = 0.6 # motor step size
+azimuth_step_deg = 0.9 # azimuth step size
 
 with h5py.File(h5_path, "r") as f:
     ds = f["rectangular_images"]
@@ -25,14 +25,16 @@ with h5py.File(h5_path, "r") as f:
     ax.set_aspect('equal')
 
     for az_idx in range(az_steps):
+        print(f"Reading {az_idx} azimuth")
         for motor_idx in range(motor_steps):
             azimuth_deg = az_idx * azimuth_step_deg
             motor_deg = motor_idx * motor_step_deg
-            rect = patches.Rectangle((motor_deg, azimuth_deg), tile_width_deg, tile_height_deg,
+            rect = patches.Rectangle((motor_deg, azimuth_deg), tile_height_deg, tile_width_deg,
                                       linewidth=0.5, edgecolor=None, facecolor = None)
             ax.add_patch(rect)
 
     for az_idx in range(az_steps):
+        print(f"Placing {az_idx} azimuth")
         for motor_idx in range(motor_steps):
             img = ds[az_idx, motor_idx]
 
@@ -42,6 +44,7 @@ with h5py.File(h5_path, "r") as f:
             if img_f32.max() > 0:
                 img_f32 /= img_f32.max()
             img_u8 = (img_f32 * 255).astype(np.uint8)
+            img_u8 = cv2.rotate(img_u8, cv2.ROTATE_90_CLOCKWISE)
 
             #resize to match image's angular size
             display_H = int(tile_height_deg * 400)
@@ -51,11 +54,11 @@ with h5py.File(h5_path, "r") as f:
             #compute degree placement
             motor_deg = motor_idx * motor_step_deg
             azimuth_deg = az_idx * azimuth_step_deg
-            extent = [motor_deg, motor_deg + tile_width_deg,azimuth_deg, azimuth_deg + tile_height_deg]
+            extent = [motor_deg, motor_deg + tile_height_deg,azimuth_deg, azimuth_deg + tile_width_deg]
 
             #put image over rectangle
             ax.imshow(img_resized, cmap='gray', extent=extent, origin='lower', aspect=1, zorder=1)
-
+            
     ax.set_xlabel("Motor Angle (deg)")
     ax.set_ylabel("Azimuth Angle (deg)")
     ax.set_title("Actual Images on Grid")
